63e0edef88494004542cdc2ed4eb446cc87dd56a
Rachel Catches-Ford <RachelACatchesFord@gmail.com>
2021-11-23 09:30:27 -0700

    makes it look pretty

--- Search-DCsForComputer.ps1
+++ Search-DCsForComputer.ps1
@@ -7,23 +7,25 @@ Param(
 
 $DomainControllers = Get-ADDomainController -Filter *  | Where-Object{($_.Name -notlike 'DHSAZDC11') -and ($_.Name -notlike 'CDHS-ESSP-DC1') -and ($_.Name -notlike 'DHSAZDC12')}
 $DomainControllers | ForEach-Object{
-    Write-Output "Searching DC $($_) for Computer $($Computer)"
-    $ADSearch = Get-ADComputer -Identity "$Computer" -Property * -Server $_ 
-    $Results = $ADSearch | select Name, DistinguishedName, Description
+Write-Output "Searching DC $($_) for Computer $($Computer)"
+$ADSearch = Get-ADComputer -Identity "$Computer" -Property * -Server $_ 
+$Results = $ADSearch | select Name, DistinguishedName, Description
 
-    Write-Output "$DC Found $Results"
+Write-Output "$DC Found $Results"
 
-    if ($Delete -eq $true) {
+    if ($Delete -eq $true){
         Write-Output "Removing $($Computer) from DC $($_)"
         $ADSearch | Remove-ADObject -Recursive -Verbose -Confirm:$false
     }
 
-    if ($CMDelete -eq $true) {
-        Write-Output "Removing $($Computer) from SCCM"
+    if ($CMDelete -eq $true){
+        # Import the module for SCCM
         Import-Module ($env:SMS_ADMIN_UI_PATH.Substring(0,$env:SMS_ADMIN_UI_PATH.Length - 5) + '\ConfigurationManager.psd1') | Out-Null
+
         ## Get the CMSite
         # In order for this next line to work, you must login to your computer as your SafeGuard account (the same account that is running this script)
         $Site=Get-PSDrive | Where-Object{$_.Provider -like '*CMSite'}
+
         ## Mount the CMSite PS Drive
         Set-Location $Site':'
 
@@ -34,27 +36,3 @@ $DomainControllers | ForEach-Object{
 
     }
 }
-
-
-<#
-##Aaron's Script
-#Get a list of all domain controllers
-$dcs=Get-ADDomainController -Filter * | Select-Object name
-#Computer to search
-$computername = $env:COMPUTERNAME #Read-Host 'Computer Name'
-
-#Run a foreach cycle to Search a computer across all Domain Controllers
-foreach ($dc in $dcs.Name) {
-	#Try to run Get-ADComputer and write Computername - DC name, if it fails use Catch to write something else
-	Try{	
-		$temp = Get-ADComputer ($computername) -Server $dc | select Name, DistinguishedName, Description
-		Write-host "$($temp.Name) - $($dc)"
-	}Catch{
-        Write-host "Does not exist on $($dc)."
-        }
-    Finally{
-        $Time=Get-Date
-        write-host "$Time"
-    }
-}
-#>
